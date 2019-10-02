5f536fe79e8887eb96b594e0bd585b7c760873e8
zcalbrecht <zcalbrecht@gmail.com>
2019-10-01 21:09:42 -0500

    Initial commit

---
+++ app/src/main/java/com/zca/keepingscore/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,59 @@
+package com.zca.keepingscore.ui.dashboard;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.zca.keepingscore.R;
+
+public class DashboardFragment extends Fragment {
+
+    private DashboardViewModel dashboardViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(DashboardViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        final TextView textView = root.findViewById(R.id.text_dashboard);
+        dashboardViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+
+        Button button = root.findViewById(R.id.mash_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+                int defaultValue = getResources().getInteger(R.integer.saved_times_pressed_default_key);
+                int timesPressed = sharedPref.getInt(getString(R.string.saved_button_pressed_count_key), defaultValue);
+
+                int newTimesPressed = timesPressed + 1;
+
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putInt(getString(R.string.saved_button_pressed_count_key));
+                editor.commit();
+
+                textView.setText("Button has been pressed " + Integer.toString(highScore) + " times");
+            }
+        });
+
+
+
+        return root;
+    }
+}
